926c52e6b85ca07d20ea61dc51fd210542543acf
Shahnewaz Tameem <shahnewaztamim@gmail.com>
2020-10-26 01:53:18 +0600

    26-10-2020

---
+++ countUniqueValues.js
@@ -0,0 +1,20 @@
+// Implement a function called countUniqueValues, which accepts a sorted array, and counts the unique values in the array. There can be negative numbers in the array, but it will always be sorted O(n)
+
+function countUniqueValues(array) {
+  if (array.length === 0) {
+    return 0;
+  }
+  let i = 0;
+  for (let j = 1; j < array.length; j++) {
+    if (array[i] !== array[j]) {
+      i++;
+      array[i] = array[j];
+    }
+  }
+  return i + 1;
+}
+
+console.log(countUniqueValues([1, 1, 1, 1, 1, 2])); // 2
+console.log(countUniqueValues([1, 2, 3, 4, 4, 4, 7, 7, 12, 12, 13])); // 7
+console.log(countUniqueValues([]));// 0
+console.log(countUniqueValues([-2, -1, -1, 0, 1])); // 4
---
+++ sumZero.js
@@ -0,0 +1,34 @@
+//Write a function called sumZero which accepts a sorted array of integers. The function should find the first pair where the sum is 0. Return an array that includes both values that sum to zero or undefined if a pair does not exist
+
+
+// naive approach complexity O(n^2)
+
+// function sumZero(arr) {
+//   for (let i = 0; i < arr.length; i++) {
+//     for (let j = i + 1; j < arr.length; j++) {
+//       if (arr[i] + arr[j] === 0) {
+//         return [arr[i], arr[j]]
+//       }
+//     }
+//   }
+// }
+
+// using multiple pointer pattern O(n)
+function sumZero(arr) {
+  let left = 0;
+  let right = arr.length - 1;
+
+  while (left < right) {
+    let sum = arr[left] + arr[right];
+    if (sum === 0) {
+      return [arr[left], arr[right]];
+    } else if (sum > 0) {
+      right--;
+    } else {
+      left++
+    }
+  }
+}
+console.log(sumZero([-3, -2, -1, 0, 1, 2, 3]));  // [-3,3] 
+console.log(sumZero([-2, 0, 1, 3]));  // undefined
+console.log(sumZero([1, 2, 3]));  // undefined
